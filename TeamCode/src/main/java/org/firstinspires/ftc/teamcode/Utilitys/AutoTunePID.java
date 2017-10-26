fbfe7a09d14787aed8d7130bbe4ade79c93c9a25
Ethan Hampton <ethan.hampton@msd.oregonk-12.net>
2017-10-25 17:45:52 -0700

    Added PID and hopefully auto tuned PID controllers that we can use to precision align our robot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilitys/AutoTunePID.java
@@ -0,0 +1,192 @@
+package org.firstinspires.ftc.teamcode.Utilitys;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.TreeMap;
+
+/**
+ * Created by ethan.hampton on 10/25/2017.
+ * <p>
+ * Experimental PID auto tuner
+ */
+public class AutoTunePID extends LinearOpMode {
+    protected static DcMotor motorFR;
+    protected static DcMotor motorFL;
+    protected static DcMotor motorBR;
+    protected static DcMotor motorBL;
+    protected static volatile double[] rollAngle = new double[2], pitchAngle = new double[2], yawAngle = new double[2];
+    protected double kP = 0.06;
+    protected double kI = 0.02;
+    protected double kD = 0.005;
+    private double dt;
+    private double prevError;
+    private double error = 0;
+    private double iError = 0;
+    private double dError;
+    private double time = getRuntime();
+    private double target_heading;
+    private TreeMap<Double, Double> time_displacement = new TreeMap<Double, Double>();
+
+
+    public void resetPID() throws InterruptedException {
+        if (!opModeIsActive())
+            return;
+        if (Math.abs(target_heading - getGyroYaw() + 360) < Math.abs(target_heading - getGyroYaw()))
+            error = target_heading - getGyroYaw() + 360;
+        else
+            error = target_heading - getGyroYaw();
+        time = getRuntime();
+        iError = 0;
+    }
+
+    public double tune_PID(double timeout) throws InterruptedException {
+        long timer = (long) (timeout * Math.pow(10, 3)) + System.currentTimeMillis();
+        double kP = 0.01; // start value. It IS too small.
+        //double max;
+        target_heading = 10;
+        resetPID();
+        while (!isTuned(kP, 5) && System.currentTimeMillis() < timer) {
+            kP *= 2;
+            target_heading = (target_heading == 10 ? 0 : 10);
+        }
+        kP /= 2;
+        while (!isTuned(kP, 5) && System.currentTimeMillis() < timer) {
+            kP *= 1.1;
+            target_heading = (target_heading == 10 ? 0 : 10);
+        }
+        kP /= 1.1;
+        while (!isTuned(kP, 5) && System.currentTimeMillis() < timer) {
+            kP += .0005;
+            target_heading = (target_heading == 10 ? 0 : 10);
+        }
+        kP -= .0005;
+        Iterator<Double> it = time_displacement.keySet().iterator();
+        double Tu = 0; // Tu is the period of the oscillation. T meaning period in physics.
+        double previous = it.next();
+        //finding the average period
+        //note: next-previous is one half period... so needs to be doubled at the end
+        while (it.hasNext()) {
+            double next = it.next();
+            Tu += next - previous;
+            previous = next;
+        }
+        Tu = Tu * 2 / (time_displacement.size() - 1); // calculate period based off of some number of half-periods
+        this.kP = 0.6 * kP;
+        this.kI = 2 * this.kP / Tu;
+        this.kD = this.kP * Tu / 8;
+        return time_displacement.get(previous); // returns the amplitude of the
+    }
+
+    public double getGyroYaw() throws InterruptedException {
+        return 0;// TODO: 10/25/2017 ADD GYRO HEADING BACK IN
+    }
+
+    public double get_PID() throws InterruptedException {
+        return get_PID(getGyroYaw(), kP, kI, kD);
+    }
+
+    public double get_PID(double gyro) throws InterruptedException {
+        return get_PID(gyro, kP, kI, kD);
+    }
+
+    public double get_PID(double kP, double kI, double kD) throws InterruptedException {
+        return get_PID(getGyroYaw(), kP, kI, kD);
+    }
+
+    public double get_PID(double gyro, double kP, double kI, double kD) throws InterruptedException {
+        if (!opModeIsActive())
+            return 0;
+        dt = getRuntime() - time;
+        time = getRuntime();
+        prevError = error;
+        if (Math.abs(target_heading - gyro + 360) < Math.abs(target_heading - gyro))
+            error = target_heading - gyro + 360;
+        else
+            error = target_heading - gyro;
+        dError = (error - prevError) / dt;
+        //make this a reimann right sum if needed to improve speed at the cost of accuracy
+        iError = Range.clip(iError + 0.5 * (prevError + error) * dt, -125, 125) * 0.99; // a trapezoidal approximation of the integral.
+        return kP * error + kD * dError + kI * iError;
+    }
+
+    public boolean isTuned(double kP, double timeout) throws InterruptedException {
+        //TODO: add debug log info for debugging
+        time_displacement = new TreeMap<Double, Double>();
+        long timer = (long) (timeout * Math.pow(10, 3)) + System.currentTimeMillis();
+        resetStartTime();
+        double PID_change;
+        double right;
+        double left;
+        double new_yaw = getGyroYaw();
+        resetPID();
+        double sign = Math.signum(get_PID(kP, 0, 0));
+        while (System.currentTimeMillis() < timer) {
+            while (new_yaw == getGyroYaw()) {
+                waitOneFullHardwareCycle();
+            }
+            new_yaw = getGyroYaw();
+            PID_change = get_PID(new_yaw, kP, 0, 0);
+            if (sign != Math.signum(PID_change)) {
+                time_displacement.put(getRuntime(), error);
+                sign *= -1;
+            }
+            //keep the absolute value of the motors above 0.3 and less than 0.7
+            right = Range.clip(PID_change, -1, 1);
+            left = -right;
+            setRightPower(right);
+            setLeftPower(left);
+            //DbgLog.error(String.format("kP: %.4f", kP));
+            //DbgLog.error(time_displacement.toString());
+            //DbgLog.error(String.format("k*error: %.2f, error: %.2f", kP * error, error));
+            //DbgLog.error(String.format("gyro:%.2f, target:%.2f, PID (right):%.2f", getGyroYaw(), target_heading, PID_change));
+        }
+        //DbgLog.error(String.format("time_displacement.size(): %d", time_displacement.size()));
+        //DbgLog.error(time_displacement.toString());
+        if (time_displacement.size() < 3)
+            return false;
+        double max = Double.MIN_VALUE;
+        double min = Double.MAX_VALUE;
+        for (Double aDouble : time_displacement.keySet()) {
+            double next = Math.abs(time_displacement.get(aDouble)); // measuring amplitude differentials.
+            max = Math.max(max, next);
+            min = Math.min(min, next);
+        }
+        //DbgLog.error(String.format("max-min: %.2f", max - min));
+        return max - min <= 2;
+    }
+
+    private void setLeftPower(double power) {
+        if (!opModeIsActive())
+            return;
+        motorBL.setPower(Range.clip(-power, -1, 1));
+        motorFL.setPower(Range.clip(-power, -1, 1));
+    }
+
+    private void setRightPower(double power) {
+        if (!opModeIsActive())
+            return;
+        motorBR.setPower(Range.clip(power, -1, 1));
+        motorFR.setPower(Range.clip(power, -1, 1));
+    }
+
+    public void runOpMode() throws InterruptedException {
+        motorFR = hardwareMap.dcMotor.get("rf");
+        motorBR = hardwareMap.dcMotor.get("rb");
+        motorFL = hardwareMap.dcMotor.get("lf");
+        motorBL = hardwareMap.dcMotor.get("lb");
+        //navx = new NavXDevice(hardwareMap, "dim", 0);
+        waitForStart();
+        tune_PID(120);
+
+        //DbgLog.error("kP: %.5f, kI: %.5f, kD: %.5f", kP, kI, kD);
+        telemetry.addData("hi", String.format(Locale.US, " kP: %.5f, kI: %.5f, kD: %.5f", kP, kI, kD));
+        //kP = 0.01061
+        //kI = 0.02116
+        //kD = 0.00133
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilitys/PID.java
@@ -0,0 +1,241 @@
+package org.firstinspires.ftc.teamcode.Utilitys;
+
+
+/**
+ * PID Targeting
+ */
+public class PID {
+    protected PIDCoefficients coefficients = new PIDCoefficients(0.005, 0, 0);  // PID constant multipliers
+    protected double setpoint = 0;
+    protected double processValue = 0;  // actual position (Process Value)
+    protected double error = 0;   // how much SP and PV are diff (SP - PV)
+    protected double integral = 0; // curIntegral + (error * Delta Time)
+    protected double derivative = 0;  //(error - prev error) / Delta time
+    protected double previousError = 0; // error from last time (previous Error)
+    protected double dt = 100.0; // delta time
+    protected double output = 0; // the drive amount that effects the PV.
+    protected double outputFlattened = 0; // drive power flattened by the accel and decel
+    protected double outputLast = 0; // last output power
+    protected double maxChangePositive = 0; // maximum change in output per second
+    protected double maxChangeNegative = 0; // maximum change in output per second
+    protected double minOutput = -1; // maximum change in output per second
+    protected double maxOutput = 1; // maximum change in output per second
+    protected double minPower = 0; // minimum power for anti-stall protection
+
+    public PID() {
+        this.coefficients = new PIDCoefficients(0.05, 0, 0);
+    }
+
+    public PID(double p, double i, double d) {
+        this.coefficients = new PIDCoefficients(p, i, d);
+    }
+
+    public PID(PIDCoefficients coefficients) {
+        this.coefficients = coefficients;
+    }
+
+    public void setCoefficients(double p, double i, double d) {
+        coefficients.p = p;
+        coefficients.i = i;
+        coefficients.d = d;
+    }
+
+    public void addMeasurement(double measuredValue, double timeDelta) {
+        this.processValue = measuredValue;
+        this.dt = timeDelta;
+        update();
+    }
+
+    public double getSetpoint() {
+        return this.setpoint;
+    }
+
+    public void setSetpoint(double setpoint) {
+        this.setpoint = setpoint;
+    }
+    /*
+    public void setSetpointDistance(MotorInfo motorInfo, double distance, Units.Distance distanceUnit) {
+        setSetpoint(Units.Distance.convertToAngle(distance,
+                motorInfo.getEffectiveWheelRadius(distanceUnit),
+                distanceUnit, Units.Distance.FEET, Units.Angle.ENCODER_COUNTS));
+    }
+    */
+
+    /**
+     * Gets the most recent error - the difference between the setpoint and the measured value
+     *
+     * @return setpoint - measured value
+     */
+    public double getError() {
+        return error;
+    }
+
+    /**
+     * Get the integral value, or the sum of the recent error values
+     *
+     * @return Integral value I
+     */
+    public double getIntegralValue() {
+        return integral;
+    }
+
+    /**
+     * Get the derivative value, or how much the error is changing
+     *
+     * @return The slope of the error values
+     */
+    public double getDerivativeValue() {
+        return derivative;
+    }
+
+    /**
+     * Gets the error value from the previous measurement
+     *
+     * @return The previous error value
+     */
+    public double getPreviousError() {
+        return previousError;
+    }
+
+    public PIDCoefficients getCoefficients() {
+        return coefficients;
+    }
+
+    public void setCoefficients(PIDCoefficients coefficients) {
+        coefficients.ff = 0.0;
+        this.coefficients = coefficients;
+    }
+
+    public double getLastTimeDelta() {
+        return dt;
+    }
+
+    public double getOutputValue() {
+        return outputFlattened;
+    }
+
+    public double getOutputValueRaw() {
+        return output;
+    }
+
+    public void setOutputRange(double min, double max) {
+        this.minOutput = min;
+        this.maxOutput = max;
+    }
+
+    public double getMaxAcceleration() {
+        return maxChangePositive;
+    }
+
+    public void setMaxAcceleration(double maxChange) {
+        this.maxChangePositive = maxChange;
+    }
+
+    public double getMaxDeceleration() {
+        return maxChangeNegative;
+    }
+
+    public void setMaxDeceleration(double maxChange) {
+        this.maxChangeNegative = Math.abs(maxChange);
+    }
+
+    public double getAntistallMinimumPower() {
+        return minPower;
+    }
+
+    public void setAntistallMinimumPower(double minPower) {
+        this.minPower = minPower;
+    }
+
+    private void update() {
+        // calculate the difference between the desired value and the actual value
+        error = setpoint - processValue;
+        // track error over time, scaled to the timer interval
+        integral = integral + (error * dt);
+        // determin the amount of change from the last time checked
+        derivative = (error - previousError) / dt;
+
+        // calculate how much drive the output in order to get to the
+        // desired setpoint.
+        output = (coefficients.p * error) + (coefficients.i * integral) + (coefficients.d * derivative);
+
+        //Clamp output
+        output = coerce(minOutput, maxOutput, output);
+        outputFlattened = output;
+
+        //Make sure the system does not move too fast
+        if ((outputFlattened - outputLast) > maxChangePositive * dt && maxChangePositive > 0)
+            outputFlattened = outputLast + (maxChangePositive * dt);
+        else if ((outputLast - outputFlattened) > maxChangeNegative * dt && maxChangeNegative > 0)
+            outputFlattened = outputLast - (maxChangeNegative * dt);
+
+        //Clamp and deadband the flattened output
+        outputFlattened = deadband(minPower, coerce(minOutput, maxOutput, outputFlattened));
+
+        // remember the error and output for the next time around.
+        previousError = error;
+        outputLast = outputFlattened;
+    }
+
+    public static class PIDCoefficients {
+        double p;
+        double i;
+        double d;
+        double ff;
+
+        public PIDCoefficients(double p, double i, double d) {
+            this.p = p;
+            this.i = i;
+            this.d = d;
+            this.ff = 0.0;
+        }
+
+        public PIDCoefficients(double p, double i, double d, double ff) {
+            this.p = p;
+            this.i = i;
+            this.d = d;
+            this.ff = ff;
+        }
+
+        public double p() {
+            return p;
+        }
+
+        public double i() {
+            return i;
+        }
+
+        public double d() {
+            return d;
+        }
+
+        public double ff() {
+            return ff;
+        }
+    }
+
+    /**
+     * Gives a "deadzone" where any value less
+     * than this would return zero.
+     *
+     * @param deadband Maximum value that returns zero
+     * @param value    Value to test
+     * @return Deadbanded value
+     */
+    public static double deadband(double deadband, double value) {
+        return (Math.abs(value) > deadband) ? value : 0;
+    }
+
+    /**
+     * Forces a numerical value to be between a min
+     * and a max.
+     *
+     * @param min   If less than min, returns min
+     * @param max   If greater than max, returns max
+     * @param value Value to test
+     * @return Coerced value
+     */
+    public static double coerce(double min, double max, double value) {
+        return (value > max) ? max : (value < min) ? min : value;
+    }
+}
