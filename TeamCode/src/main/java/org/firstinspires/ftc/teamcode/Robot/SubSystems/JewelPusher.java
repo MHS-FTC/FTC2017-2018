cf96396a3b5972c32a50d78534d17dd72ff8f284
Ethan Hampton <ethan.hampton@msd.oregonk-12.net>
2018-02-01 15:02:54 -0800

    Dead zoned jewel color sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/SubSystems/JewelPusher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/SubSystems/JewelPusher.java
@@ -82,30 +82,38 @@ public class JewelPusher extends SubSystem {
         }
     }
 
-    // TODO: 1/31/2018 Change so there is a dead zone for hue
+    // TODO: 1/31/2018 Test hue dead zone
     public Direction whereToHit(Direction currentlyReading, Team team) {
         Color.RGBToHSV((color.red() * 255) / 800, (color.green() * 255) / 800, (color.blue() * 255) / 800, colorValues);
+
         /* is true if the color sensor is less than 60 hue or greater than 300 then it is probably red,
          implies that the ball it is facing at is red */
-        boolean isRed = colorValues[0] < 30 || colorValues[0] > 330;//about a 30 number
-        if (team.equals(Team.RED_TEAM)) {
-            //if we are looking at a red ball and we are on the red team, hit the other one
-            if (isRed) {
-                return Direction.getOpposite(currentlyReading);
-            } else {
-                //if we are on red and we see blue, hit it
-                return currentlyReading;
+        boolean isRed = (colorValues[0] < 30 || colorValues[0] > 330);//about a 30 number
+        boolean isInvalid = (colorValues[0] > 30 && colorValues[0] < 180)
+                && (colorValues[0] > 290 && colorValues[0] < 330);//between 30 and 180 is definitely invalid, as is between 290 and 330
+
+        if (!isInvalid) {//insure not in invalid range, in which case we are not sure
+            if (team.equals(Team.RED_TEAM)) {
+                //if we are looking at a red ball and we are on the red team, hit the other one
+                if (isRed) {
+                    return Direction.getOpposite(currentlyReading);
+                } else {
+                    //if we are on red and we see blue, hit it
+                    return currentlyReading;
+                }
+            } else if (team.equals(Team.BLUE_TEAM)) {
+                //if we are looking at a blue ball and we are on the blue team, hit the other one
+                if (!isRed) {
+                    return Direction.getOpposite(currentlyReading);
+                } else {
+                    //if we are on blue and we see red, hit it
+                    return currentlyReading;
+                }
             }
-        } else if (team.equals(Team.BLUE_TEAM)) {
-            //if we are looking at a blue ball and we are on the blue team, hit the other one
-            if (!isRed) {
-                return Direction.getOpposite(currentlyReading);
-            } else {
-                //if we are on blue and we see red, hit it
-                return currentlyReading;
-            }
+            return Direction.UNKNOWN;
+        } else {
+            return Direction.MIDDLE;//return middle if we are not sure it is a color
         }
-        return Direction.UNKNOWN;
     }
 
     public void reset() {
