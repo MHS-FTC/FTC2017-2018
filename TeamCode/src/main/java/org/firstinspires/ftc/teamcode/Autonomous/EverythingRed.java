1066acaa75b7b03bffcab4836236843496c1ac1d
Ethan Hampton <ethan.hampton@msd.oregonk-12.net>
2018-02-10 14:46:04 -0800

    Fixed red everything

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/EverythingRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/EverythingRed.java
@@ -1,3 +1,4 @@
+
 package org.firstinspires.ftc.teamcode.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -10,24 +11,22 @@ import org.firstinspires.ftc.teamcode.Robot.Robot;
 import org.firstinspires.ftc.teamcode.Utilitys.Team;
 
 /**
- * Created by byron.nice on 2/9/2018.
- *
- * does everything for red
+ * Created by Ethan Hampton
+ * Does everything for Red
  */
 
 @Autonomous(name = "RED: Everything", group = "Red")
 public class EverythingRed extends OpMode {
     private AutonomousBase auto = new AutonomousBase();
-    private Robot robot;
-
-    private Module[][] modules = {
+    private Robot bot;
+    private final Module[][] steps = new Module[][]{
             {new JewelHitter().setTeam(Team.RED_TEAM)},
             {new Cryptograms()}, //Decrypts Cryptogram
-            {new CallFunction().setFunction(()-> robot.forklift.close())}, //Close claws
+            {new CallFunction().setFunction(() -> bot.forklift.close())}, //Close claws
             {new Wait().setWaitTime(300)},//Wait
-            {new CallFunction().setFunction(()-> robot.forklift.raise(0.4))}, //Raise Forklift
+            {new CallFunction().setFunction(() -> bot.forklift.raise(0.4))}, //Raise Forklift
             {new Wait().setWaitTime(500)},//Wait
-            {new CallFunction().setFunction(()-> robot.forklift.raise(0.0))},//Stop Forklift
+            {new CallFunction().setFunction(() -> bot.forklift.raise(0.0))},//Stop Forklift
             {new Wait().setWaitTime(300)},//Wait
             {new DriveTime().setSpeeds(-0.4,0,0).setTime(2500)},
             {new DriveTime().setSpeeds(0,0.5,0).setTime(2800)},
@@ -38,21 +37,23 @@ public class EverythingRed extends OpMode {
                     new DriveTime().setSpeeds(0,0,-0.6).setTime(550),
             },//the three possible positions for the cryptobox
             {new EncoderDrive().setDistances(5,5).setSpeed(0.3)},//Move Forward
-            {new CallFunction().setFunction(() -> robot.forklift.open())}, //Open claws
+            {new CallFunction().setFunction(() -> bot.forklift.open())}, //Open claws
             {new Wait().setWaitTime(300)},//wait to open before continuing back
             {new DriveTime().setSpeeds(-0.2,0,0).setTime(600)},//Moves back
-            {new Wait()}
+            {new Wait()},
     };
 
     @Override
     public void init() {
-        robot = new Robot();
+        bot = new Robot();
         auto.setTelemetry(telemetry);
-        auto.init(hardwareMap, robot, modules);
+        auto.init(hardwareMap, bot, steps);
     }
 
     @Override
     public void loop() {
-        telemetry.addData("Time", robot.getTimeMilliseconds());
+        auto.loop();
+
+        telemetry.addData("Test", bot.drive.isFunctioning());//Add telemetry
     }
 }
